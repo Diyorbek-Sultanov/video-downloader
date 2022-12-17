695bdfeab5eb7ceb0b82cd2027411188b680d40c
Diyorbek-Sultanov <diyorbeksultonov1@mail.ru>
2022-12-17 22:26:51 +0500

    request function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	const navBtn = document.getElementById('nav__btn')
 	const navClose = document.getElementById('nav__close')
 	const nav = document.getElementById('my-nav')
+
 	if (navBtn) {
 		navBtn.addEventListener('click', () => {
 			nav.classList.add('show-menu')
@@ -39,3 +40,68 @@ window.addEventListener('DOMContentLoaded', () => {
 		links.forEach(link => link.classList.remove('nav-active'))
 	}
 })
+
+// Getting requests
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const getVideo = document.getElementById('video')
+const videoDetail = document.getElementById('video-detail')
+const convertNext = document.getElementById('convert-next')
+const notFound = document.getElementById('not-found')
+const loader = document.getElementById('loader')
+
+loader.style.display = 'none'
+
+form.addEventListener('submit', e => formHandler(e))
+
+const formHandler = e => {
+	e.preventDefault()
+
+	const formData = new FormData(form)
+	let obj = {}
+
+	formData.forEach((value, key) => {
+		obj[key] = value
+	})
+
+	if (!obj) {
+		notFound.classList.add('notFound-active')
+	} else {
+		loader.style.display = 'block'
+		fetchingLink(obj)
+			.then(() => {
+				loader.style.display = 'none'
+			})
+			.then(() => {
+				videoDetail.classList.add('videoDetail-active')
+			})
+			.catch(e => {
+				throw new Error(e)
+			})
+
+		getVideo.style.display = 'none'
+
+		convertNext.addEventListener('click', () => {
+			getVideo.style.display = 'block'
+			videoDetail.classList.remove('videoDetail-active')
+			input.value = ''
+		})
+	}
+}
+
+const fetchingLink = async obj => {
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+		},
+		body: JSON.stringify(obj),
+	}
+
+	const response = await fetch('http://localhost:4444/links', options)
+	const data = await response.json()
+}
+
+const loaderBlock = () => {
+	document.body.classList.add()
+}
